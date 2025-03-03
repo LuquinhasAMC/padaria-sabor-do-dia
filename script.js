24918dbf55dd9fdc78c9680f2dc02500645fc9f6
LuquinhasAMC <168360461+LuquinhasAMC@users.noreply.github.com>
2025-03-02 21:47:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function btI(id) {
+    document.getElementById("inicioS").scrollIntoView({behavior: "smooth"});
+}
+
+function btD(id) {
+    document.getElementById("delicias").scrollIntoView({behavior: "smooth"});
+}
+
+function pedir(id) {
+    document.getElementById("pedidos").scrollIntoView({behavior: "smooth"});
+}
+
+function btC(id) {
+    document.getElementById("contato").scrollIntoView({behavior: "smooth"})
+}
+
+function Fpedir() {
+    alert("Pedido finalizado");
+}
